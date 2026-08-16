106e05fb1a730f5b46351127fcb73e0fe5615f25
wikifolio-user <marsianer333@gmail.com>
2026-08-16 22:39:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -768,6 +768,7 @@ try:
             xaxis=dict(showgrid=True, gridcolor="#1A1A1A", type="date", tickfont=dict(color="#A1A1AA")),
             yaxis=dict(showgrid=True, gridcolor="#1A1A1A", side="right", tickfont=dict(color="#A1A1AA"), dtick=2000, tickformat=",.0f"),
             hovermode="x unified",
+            separators=",.",
         )
         st.plotly_chart(fig_wealth, width="stretch")
 
